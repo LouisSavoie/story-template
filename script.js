f2852a9d6bbbac3475f2374fa0bc3b0342a14d4f
Louis Savoie <thelousavoie@gmail.com>
2020-11-20 20:22:39 -0500

    add skeleton html /css and JS DOM elements

---
+++ script.js
@@ -0,0 +1,8 @@
+// DOM ELEMENTS
+const titleDOM = document.getElementById('title');
+const productsDOM = document.getElementById('products');
+const reviewsDOM = document.getElementById('reviews');
+const aboutDOM = document.getElementById('about');
+const staffDOM = document.getElementById('staff');
+const mapDOM = document.getElementById('map');
+const contactDOM = document.getElementById('contact');
